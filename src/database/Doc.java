ce4dc9d0a3db13f61d5f726f98205374ebeb93e0
Anas Megahed <anasmegahed@gmail.com>
2019-12-08 16:20:32 +0200

    Database initial hierarchy

---
+++ src/database/Doc.java
@@ -0,0 +1,21 @@
+package database;
+
+public class Doc implements Target {
+	private String path;
+	
+	@Override
+	public void fetchPage() {
+		
+	}
+
+	@Override
+	public void parsePage() {
+
+	}
+
+	@Override
+	public void saveData() {
+
+	}
+
+}
---
+++ src/database/Html.java
@@ -0,0 +1,20 @@
+package database;
+
+public class Html implements Target {
+	private String url;
+	
+	@Override
+	public void fetchPage() {
+		
+	}
+
+	@Override
+	public void parsePage() {
+
+	}
+
+	@Override
+	public void saveData() {
+
+	}
+}
